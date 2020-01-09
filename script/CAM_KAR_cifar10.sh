c2bc7c0b102d1e000a9b1b95d3c2cb012f20e3f6
TooTouch <wogur379@gmail.com>
2020-01-09 15:28:09 +0900

    add bash file

--- script/CAM_KAR_cifar10.sh
+++ script/CAM_KAR_cifar10.sh
@@ -1,9 +1,9 @@
-nohup python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.1 > print.log &
-nohup python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.2 > print.log &
-nohup python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.3 > print.log &
-nohup python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.4 > print.log &
-nohup python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.5 > print.log &
-nohup python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.6 > print.log &
-nohup python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.7 > print.log &
-nohup python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.8 > print.log &
-nohup python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.9 > print.log &
+python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.1 && \
+python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.2 && \
+python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.3 && \
+python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.4 && \
+python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.5 && \
+python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.6 && \
+python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.7 && \
+python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.8 && \
+python -u ../code/main.py --eval=KAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.9
--- script/CAM_ROAR_cifar10.sh
+++ script/CAM_ROAR_cifar10.sh
@@ -1,9 +1,9 @@
-python ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.1
-python ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.2
-python ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.3
-python ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.4
-python ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.5
-python ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.6
-python ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.7
-python ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.8
-python ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.9
+python -u ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.1 && \
+python -u ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.2 && \
+python -u ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.3 && \
+python -u ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.4 && \
+python -u ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.5 && \
+python -u ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.6 && \
+python -u ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.7 && \
+python -u ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.8 && \
+python -u ../code/main.py --eval=ROAR --target=cifar10 --attention=CAM --method=CAM --ratio=0.9
